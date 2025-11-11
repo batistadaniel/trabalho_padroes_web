1b226a0c0cfdf1e3dee343123eb99089d4aa6975
batistadaniel <batistanegobiz@gamil.com>
2025-11-11 11:09:08 -0300

    Atualizaoes com JS

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+async function carregarPagina(pagina) { // funcao assincrona, significa que pode demorar
+    const section = document.getElementById('conteudo'); // pega o elemento da section onde sera carregado o conteudo
+    try {
+        const resposta = await fetch(pagina); // espera a resposta da busca da pagina
+        section.innerHTML = await resposta.text(); // insere o resposta na section do conteudo
+    } catch (erro) {
+        section.innerHTML = "Erro ao carregar a página.";
+    }
+}
+
+// carrega a home inicialmente
+carregarPagina('../../index.html');
+
+// Ao clicar no menu, troca o conteúdo
+document.querySelectorAll('nav ul li a').forEach(link => { // pega os elementos a serem clicados e percorre
+    link.addEventListener('click', e => { // adiciona o eventode clique
+        e.preventDefault(); // evita recarregar a porra da pagina
+        const pagina = e.target.getAttribute('data-page'); // e.target significa o elemento clicado "a"
+        carregarPagina(pagina);
+    });
+});
