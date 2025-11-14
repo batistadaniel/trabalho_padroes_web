54b5ef2e8bf66bc16ab422a252c5f75c9f8e6026
batistadaniel <batistanegobiz@gmail.com>
2025-11-14 16:00:59 -0300

    Atulizacao 14-11-2025 16-0

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,34 @@
+// pega os id's do html
+const btnMenu = document.getElementById('btn-menu');
+const menuMobile = document.getElementById('menu-mobile');
+const main = document.getElementById('main');
+
+// evento de quando clicar no botao do menu
+btnMenu.addEventListener("click", () => {
+    menuMobile.classList.toggle('aberto'); // abre e fecha
+});
+
+// percorre todos os links um a um
+menuMobile.querySelectorAll("a").forEach(link => {
+    // evento de quando clicar em qualquer link tbm abre e fecha
+    link.addEventListener("click", () => {
+        menuMobile.classList.remove("aberto"); // abre e fecha
+    });
+});
+
+// gambiarra marota para quando clicar fora, ou seja, na area onde tem o "main"
+main.addEventListener("click", () => {
+    menuMobile.classList.remove("aberto"); // abre e fecha
+} )
+
+/*----------------------------------------------------------------------------------------------- */
+
 async function carregarPagina(pagina) { // funcao assincrona, significa que pode demorar
     const section = document.getElementById('conteudo'); // pega o elemento da section onde sera carregado o conteudo
-    try {
+    try { // tente execultar algo
         const resposta = await fetch(pagina); // espera a resposta da busca da pagina
         section.innerHTML = await resposta.text(); // insere o resposta na section do conteudo
-    } catch (erro) {
+    } catch (erro) {  // caso a tentativa der erro
         section.innerHTML = "Erro ao carregar a página.";
     }
 }
@@ -11,11 +36,12 @@ async function carregarPagina(pagina) { // funcao assincrona, significa que pode
 // carrega a home inicialmente
 carregarPagina('src/pages/home.html');
 
-// Ao clicar no menu, troca o conteúdo
-document.querySelectorAll('nav ul li a').forEach(link => { // pega os elementos a serem clicados e percorre
-    link.addEventListener('click', e => { // adiciona o eventode clique
-        e.preventDefault(); // evita recarregar a porra da pagina
-        const pagina = e.target.getAttribute('data-page'); // e.target significa o elemento clicado "a"
-        carregarPagina(pagina);
+// ao clicar no menu, troca o conteúdo
+document.querySelectorAll('nav ul li a').forEach(link => { // pega os elementos a serem clicados e percorre um por um
+    link.addEventListener('click', e => { // adiciona o evento de clique
+        e.preventDefault(); // evita recarregar a porra da pagina de maneira padrao
+        const pagina = e.target.getAttribute('data-page'); // e.target significa o elemento clicado, tag "a"
+        carregarPagina(pagina); // chama a danada da funcao 
     });
 });
+
